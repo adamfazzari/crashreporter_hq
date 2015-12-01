412f8ba433ea2dd5bfe5aed9b41f4e5dd517a2c4
clobo <clobo@sensoft.ca>
2015-12-01 09:33:41 -0500

    make UPLOAD_FOLDER if it doesn't exist

--- crashreporter/hq/config.py
+++ crashreporter/hq/config.py
@@ -5,8 +5,10 @@ import os
 HQ_FOLDER = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_FOLDER = os.path.join(HQ_FOLDER, 'templates')
 STATIC_FOLDER = os.path.join(HQ_FOLDER, 'static')
-UPLOAD_FOLDER = os.path.join(HQ_FOLDER, 'reports')
 PYGMENTS_CSS_FILE = os.path.join(TEMPLATE_FOLDER, 'syntax.css')
+UPLOAD_FOLDER = os.path.join(HQ_FOLDER, 'reports')
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
 
 # Cross-Site Request Forgery prevention
 WTF_CSRF_ENABLED = True
