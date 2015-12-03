07fcc0cf7cec31bcfc6073b1e60a204da1872d03
Calvin <calvinlobo@gmail.com>
2015-12-02 21:57:26 -0500

    changed password in form to PasswordField

--- crashreporter/hq/forms.py
+++ crashreporter/hq/forms.py
@@ -1,9 +1,8 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, BooleanField
+from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(Form):
     email = StringField('email', validators=[DataRequired()])
-    password = StringField('password', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    password = PasswordField('password', validators=[DataRequired()])
