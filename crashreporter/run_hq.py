bc50d51421c0ad714679d2799d4633dca54789ba
clobo <clobo@sensoft.ca>
2015-12-04 09:06:29 -0500

    added command line interface to run_hq

--- crashreporter/run_hq.py
+++ crashreporter/run_hq.py
@@ -2,4 +2,15 @@ __author__ = 'calvin'
 
 from hq import app
 
-app.run(debug=True)
+import argparse
+
+parser = argparse.ArgumentParser(description='Crash Reporter HQ.')
+
+parser.add_argument('--host', metavar='host', type=str, default=None, help='Host IP')
+parser.add_argument('--port', metavar='port', type=int, default=None, help='Host port')
+parser.add_argument('--debug', metavar='debug', type=bool, default=False, help='Debug mode')
+
+args = parser.parse_args()
+
+
+app.run(args.host, args.port, debug=args.debug)
