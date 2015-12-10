75697068c90425b33481919324d6db8ac8caa964
clobo <clobo@sensoft.ca>
2015-12-10 15:55:12 -0500

    show 50 reports per page and always start off at the last page by default

--- crashreporter/hq/views/__init__.py
+++ crashreporter/hq/views/__init__.py
@@ -38,14 +38,14 @@ def view_report(report_number):
 @app.route('/')
 @flask_login.login_required
 def home():
-    PER_PAGE = 10
+    PER_PAGE = 50
     reports = get_reports()
     n_total_reports = len(reports)
     try:
-        page = int(request.args.get('page', 1))
+        page = max(1, int(request.args.get('page', n_total_reports / PER_PAGE)))
     except ValueError:
         page = 1
     reports = reports[(page-1) * PER_PAGE: page * PER_PAGE]
-    pagination = Pagination(page=page, total=n_total_reports, search=False, record_name='reports')
+    pagination = Pagination(page=page, per_page=PER_PAGE, total=n_total_reports, search=False, record_name='reports')
     html = render_template('index.html', reports=reports, user=flask_login.current_user, pagination=pagination)
     return html
