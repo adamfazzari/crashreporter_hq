f6456462e22b403ec5d073307e746e520ccd00f8
clobo <clobo@sensoft.ca>
2015-12-17 10:17:30 -0500

    renamed users.db to database.db because it now holds crash reports too

--- crashreporter/hq/config.py
+++ crashreporter/hq/config.py
@@ -6,13 +6,9 @@ HQ_FOLDER = os.path.dirname(os.path.realpath(__file__))
 TEMPLATE_FOLDER = os.path.join(HQ_FOLDER, 'templates')
 STATIC_FOLDER = os.path.join(HQ_FOLDER, 'static')
 PYGMENTS_CSS_FILE = os.path.join(TEMPLATE_FOLDER, 'syntax.css')
-UPLOAD_FOLDER = os.path.join(HQ_FOLDER, 'reports')
 TMP_FOLDER = os.path.join(HQ_FOLDER, 'tmp')
 
-DB_ABSOLUTE_PATH = 'sqlite:///' + os.path.join(HQ_FOLDER, 'tmp/users.db')
-
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
+DB_ABSOLUTE_PATH = 'sqlite:///' + os.path.join(HQ_FOLDER, 'tmp/database.db')
 
 if not os.path.isdir(TMP_FOLDER):
     os.makedirs(TMP_FOLDER)
