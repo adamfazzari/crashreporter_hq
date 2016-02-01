40d82c84aae5c642cdaf82f4ed1843958dd0711a
clobo <clobo@sensoft.ca>
2016-02-01 13:34:42 -0500

    search view now redirects to home view with filter criteria in request.args

--- crashreporter_hq/views/__init__.py
+++ crashreporter_hq/views/__init__.py
@@ -129,14 +129,18 @@ def request_group_invite():
         return redirect(url_for('groups', group=group))
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 @flask_login.login_required
 def home():
     PER_PAGE = 25
     q = get_similar_reports(return_query=True)
     if flask_login.current_user.group:
         form = SearchReportsForm()
-        reports = q.filter(CrashReport.group == flask_login.current_user.group).all()
+        reports = q.filter(CrashReport.group == flask_login.current_user.group)
+        if request.args:
+            reports = reports.filter(getattr(CrashReport, request.args['field']) == request.args['value'])
+        reports = reports.all()
+
         n_total_reports = len(reports)
         max_page = int(ceil(n_total_reports / float(PER_PAGE)))
         try:
@@ -152,25 +156,11 @@ def home():
     else:
         return redirect(url_for('groups'))
 
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET', 'POST'])
 @flask_login.login_required
 def search():
     form = SearchReportsForm()
     if flask_login.current_user.group and form.validate_on_submit():
-        PER_PAGE = 25
-        reports = CrashReport.query.filter(CrashReport.group == flask_login.current_user.group,
-                                           getattr(CrashReport, form.data['field']).contains(form.data['value'])).all()
-        n_total_reports = len(reports)
-        max_page = int(ceil(n_total_reports / float(PER_PAGE)))
-        try:
-            page = max(1, int(request.args.get('page', 1)))
-        except ValueError:
-            page = 1
-        page_rev = max_page - page + 1
-        reports = reports[(page_rev-1) * PER_PAGE: page_rev * PER_PAGE]
-        pagination = Pagination(page=page, per_page=PER_PAGE, total=n_total_reports, search=False, record_name='reports')
-        html = render_template('index.html', reports=reports, user=flask_login.current_user, pagination=pagination,
-                               form=form)
-        return html
+        return redirect(url_for('home', **form.data))
     else:
         return redirect(url_for('home'))
