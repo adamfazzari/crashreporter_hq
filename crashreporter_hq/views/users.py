1760e86b7724c66c6e7500463461244a0aa2468a
clobo <clobo@sensoft.ca>
2016-02-11 09:02:15 -0500

    give only admin access to users page

--- crashreporter_hq/views/users.py
+++ crashreporter_hq/views/users.py
@@ -9,11 +9,16 @@ from .. import app
 
 
 @app.route('/users', methods=['GET'])
+@flask_login.login_required
 def users():
 
     if request.method == 'GET':
-        user_list = User.query.all()
-        return render_template('users.html', user_list=user_list, user=flask_login.current_user)
+        if flask_login.current_user.admin:
+            user_list = User.query.all()
+            return render_template('users.html', user_list=user_list, user=flask_login.current_user)
+        else:
+            return 'Admin access only.'
+
 
 
 
