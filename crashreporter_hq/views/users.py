fcd393130a1ad3d6cb860e54b8d79432ba5f61f6
clobo <clobo@sensoft.ca>
2016-02-11 13:39:43 -0500

    added a user profile view

--- crashreporter_hq/views/users.py
+++ crashreporter_hq/views/users.py
@@ -11,13 +11,21 @@ from .. import app
 @app.route('/users', methods=['GET'])
 @flask_login.login_required
 def users():
-
     if request.method == 'GET':
-        if flask_login.current_user.admin:
-            user_list = User.query.all()
-            return render_template('users.html', user_list=user_list, user=flask_login.current_user)
+        uid = int(request.args.get('id', -1))
+        if uid and flask_login.current_user.id == uid:
+            # Show profile for the logged in user
+            return render_template('users.html', user_list=[flask_login.current_user], user=flask_login.current_user)
+        elif uid < 0:
+            if flask_login.current_user.admin:
+                user_list = User.query.all()
+                return render_template('users.html', user_list=user_list, user=flask_login.current_user)
+            else:
+                return 'User does not exist.'
+
         else:
             return 'Admin access only.'
+
 
 
 
