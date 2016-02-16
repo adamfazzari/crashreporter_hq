235eed2bae57f1f0970089cedeb0ca33b3a207d5
clobo <clobo@sensoft.ca>
2016-02-16 10:59:09 -0500

    changed argument parser of run_hq

--- run_hq.py
+++ run_hq.py
@@ -2,18 +2,42 @@ __author__ = 'calvin'
 
 from crashreporter_hq import app
 
-import argparse
+import getopt
+import sys
 
-parser = argparse.ArgumentParser(description='Crash Reporter HQ.')
+def usage():
 
-parser.add_argument('--host', metavar='host', type=str, default=None, help='Host IP')
-parser.add_argument('--port', metavar='port', type=int, default=None, help='Host port')
-parser.add_argument('--profile', metavar='profile', type=bool, default=False, help='Enable profiling')
-parser.add_argument('--debug', metavar='debug', type=bool, default=False, help='Debug mode')
+    print "Command line parameters:"
+    print " -d      Sets debug"
+    print " -h      host ip"
+    print " -p      port"
+    print " -s      enable profiling"
+    print " --help      Display help"
 
-args = parser.parse_args()
 
-if args.profile:
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "sh:p:d:")
+except getopt.GetoptError:
+    print "Invalid command line arguments"
+    usage()
+    sys.exit()
+
+app_kwargs = {'debug': False, 'host': '0.0.0.0', 'port': 5010}
+profile = False
+for opt, arg in opts:
+    if opt == "-d" and arg:
+        app_kwargs['debug'] = True
+    elif opt == "-p":
+        app_kwargs['port'] = int(arg)
+    elif opt == "-s":
+        profile = True
+    elif opt == "-h":
+        app_kwargs['host'] = arg
+    elif opt == "--help":
+        usage()
+        sys.exit()
+
+if profile:
     """
     This module provides a simple WSGI profiler middleware for finding
     bottlenecks in web application. It uses the profile or cProfile
@@ -26,8 +50,7 @@ if args.profile:
     from werkzeug.contrib.profiler import ProfilerMiddleware
 
     app.config['PROFILE'] = True
-    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions = [30])
-    app.run(args.host, args.port, debug=args.debug)
-else:
-    app.run(args.host, args.port, debug=args.debug)
+    app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+
+app.run(**app_kwargs)
 
