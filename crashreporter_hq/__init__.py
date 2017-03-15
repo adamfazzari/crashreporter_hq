0b3c9d8158d05b204eb0c37140d635a2919e74fb
clobo <clobo@sensoft.ca>
2017-03-15 15:34:10 -0400

    added profiler (default disabled)

--- crashreporter_hq/__init__.py
+++ crashreporter_hq/__init__.py
@@ -20,4 +20,36 @@ db = SQLAlchemy(app)
 # def shutdown_session(exception=None):
 #     db_session.remove()
 
+
+__PROFILER_ENABLED__ = False
+
+if __PROFILER_ENABLED__:
+    from sqlalchemy import event
+    from sqlalchemy.engine import Engine
+    import time
+    import logging
+
+    logging.basicConfig()
+    logger = logging.getLogger("myapp.sqltime")
+    logger.setLevel(logging.DEBUG)
+
+    @event.listens_for(Engine, "before_cursor_execute")
+    def before_cursor_execute(conn, cursor, statement,
+                              parameters, context, executemany):
+        context._query_start_time = time.time()
+        logger.debug("Start Query:\n%s" % statement)
+        # Modification for StackOverflow answer:
+        # Show parameters, which might be too verbose, depending on usage..
+        logger.debug("Parameters:\n%r" % (parameters,))
+
+
+    @event.listens_for(Engine, "after_cursor_execute")
+    def after_cursor_execute(conn, cursor, statement,
+                             parameters, context, executemany):
+        total = time.time() - context._query_start_time
+        logger.debug("Query Complete!")
+
+        # Modification for StackOverflow: times in milliseconds
+        logger.debug("Total Time: %.02fms" % (total * 1000))
+
 import views, models
